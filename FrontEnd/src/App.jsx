3dab8dfd284bcb0f1c5e58ef78f79e9128c89574
K.L.T. Lakchani <cst21040@std.uwu.ac.lk>
2025-12-08 21:34:31 +0530

    updates

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/NavBar'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import QuestionGenerator from './pages/questionGenerator';
 import LearningCompanion from './pages/learningCompanion';
 import Performance from './pages/Performance';
 import Home from './pages/Home';
+import Login from './pages/login';
+import Signin from './pages/signin';
 function App() {
 
   return (
@@ -17,8 +20,10 @@ function App() {
           <Route path="/generator" element={<QuestionGenerator />} />
           <Route path="/companion" element={<LearningCompanion />} />
           <Route path="/performance" element={<Performance/>} />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signin />} />
         </Routes>
+        <Footer />
     </Router>
   )
 }
