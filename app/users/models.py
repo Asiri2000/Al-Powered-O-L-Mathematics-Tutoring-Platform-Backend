f5ac9a151010a4ea4aeb8464d22b75fa6abedc2c
K.L.T. Lakchani <cst21040@std.uwu.ac.lk>
2025-12-09 12:37:18 +0530

    push

--- app/users/models.py
+++ app/users/models.py
@@ -3,7 +3,8 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
 from app.db.base import Base
 
-class User(Base):
+class User(Base): 
+    
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
